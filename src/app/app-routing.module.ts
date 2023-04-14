dc93ffa9aa78be64bc5bdf7905e518c3e8dbaca6
AyaeI <127328563+AyaeI@users.noreply.github.com>
2023-04-14 02:33:51 +0000

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { FilieresComponent } from './components/filieres/filieres.component';
 import { LoginComponent } from './components/login/login.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { AcceuilComponent } from './components/acceuil/acceuil.component';
 const routes: Routes = [
   {
     path: '', component: FilieresComponent,
-    pathMatch: 'full'
+    pathMatch:'full'
   },
   {path:'login', component:LoginComponent},
   {path:'contact', component:ContactComponent},
+  {path:'Acceuil', component:AcceuilComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FilieresComponent } from './components/filieres/filieres.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { LoginComponent } from './components/login/login.component';
+import { AcceuilComponent } from './components/acceuil/acceuil.component';
+
 
 @NgModule({
   declarations: [
@@ -14,11 +17,13 @@ import { LoginComponent } from './components/login/login.component';
     NavbarComponent,
     FilieresComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    AcceuilComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
