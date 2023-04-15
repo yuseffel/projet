9b2c4766dca102c799e2aea4846ffa36b4e81c2c
AyaeI <127328563+AyaeI@users.noreply.github.com>
2023-04-15 00:59:49 +0000

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AcceuilComponent } from './components/acceuil/acceuil.component';
 import { FilieresComponent } from './components/filieres/filieres.component';
 import { LoginComponent } from './components/login/login.component';
 import { ContactComponent } from './components/contact/contact.component';
-import { AcceuilComponent } from './components/acceuil/acceuil.component';
+
 const routes: Routes = [
-  {
-    path: '', component: FilieresComponent,
-    pathMatch:'full'
-  },
+
+  {path:'Acceuil', component:AcceuilComponent,pathMatch:'full'},
+  {path: '', component: FilieresComponent},
   {path:'login', component:LoginComponent},
-  {path:'contact', component:ContactComponent},
-  {path:'Acceuil', component:AcceuilComponent}
+  {path:'contact', component:ContactComponent}
 ];
 
 @NgModule({
